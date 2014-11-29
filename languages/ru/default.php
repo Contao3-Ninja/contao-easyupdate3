82012a2ae96cad241a1b7b9395f53c960b3d4842
Glen Langer <contao.modul@glen-langer.de>
2014-11-30 00:35:24 +0100

    add russian language from ER2, now in transifex

---
+++ languages/ru/default.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-easyupdate3/language/en/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-11-30T00:00:14+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['easyupdate3']['0'] = "easyUpdate3";
+$GLOBALS['TL_LANG']['MOD']['easyupdate3']['1'] = "Обновление Contao 3 из панели управления.";
+$GLOBALS['TL_LANG']['easyupdate3']['backBT'] = "Назад";
+$GLOBALS['TL_LANG']['easyupdate3']['headline'] = "easyUpdate обновление Contao (текущая версия: %s)";
+$GLOBALS['TL_LANG']['easyupdate3']['selectfile'] = "Выберите архив (ZIP-файл)";
+$GLOBALS['TL_LANG']['easyupdate3']['description'] = "Выберите архив из каталога (files/easyupdate3). Скрипт работает только с ZIP-архивами и не видит вложенные папки.";
+$GLOBALS['TL_LANG']['easyupdate3']['setfile'] = "Выбрать";
+$GLOBALS['TL_LANG']['easyupdate3']['files']['original'] = "Оригинальные файлы";
+$GLOBALS['TL_LANG']['easyupdate3']['files']['backup'] = "Файлы резервных копий";
+$GLOBALS['TL_LANG']['easyupdate3']['noupdate'] = "ИСКЛЮЧИТЬ файлы из обновления";
+$GLOBALS['TL_LANG']['easyupdate3']['demo'] = "Демо-файлы";
+$GLOBALS['TL_LANG']['easyupdate3']['config_legend'] = "Файлы конфигурации";
+$GLOBALS['TL_LANG']['easyupdate3']['other_legend'] = "Другие файлы";
+$GLOBALS['TL_LANG']['easyupdate3']['all'] = "Выбрать все";
+$GLOBALS['TL_LANG']['easyupdate3']['noupdatetext'] = "Вы можете выбрать файлы, ИСКЛЮЧАЕМЫЕ из обновления.<br>В резервную копию войдут все файлы включая эти.";
+$GLOBALS['TL_LANG']['easyupdate3']['updatex'] = "версия (%s) ======> версия (%s)";
+$GLOBALS['TL_LANG']['easyupdate3']['update0'] = "Будет установлена старая версия Contao. Вы уверены в необходимости? Обновление на более раннюю версию может быть не совсем простым.";
+$GLOBALS['TL_LANG']['easyupdate3']['update1'] = "Вы установите более новую версию Contao.";
+$GLOBALS['TL_LANG']['easyupdate3']['update2'] = "Вы устанавливаете туже самую версию.";
+$GLOBALS['TL_LANG']['easyupdate3']['next'] = "Следующий шаг";
+$GLOBALS['TL_LANG']['easyupdate3']['previous'] = "Вернуться назад";
+$GLOBALS['TL_LANG']['easyupdate3']['changelog']['headline'] = "Список изменений между версиями %s и %s";
+$GLOBALS['TL_LANG']['easyupdate3']['changelog']['same'] = "Будет установлена та же версия, так что ничего не изменится.";
+$GLOBALS['TL_LANG']['easyupdate3']['changelog']['no'] = "Невозможно прочитать список изменений. За дополнительной информацией посетите Contao-Community (http://www.contao.org).";
+$GLOBALS['TL_LANG']['easyupdate3']['content'] = "Содержимое архива";
+$GLOBALS['TL_LANG']['easyupdate3']['backup'] = "Резервное копирование файлов";
+$GLOBALS['TL_LANG']['easyupdate3']['backuped'] = "Резервное копирование файлов";
+$GLOBALS['TL_LANG']['easyupdate3']['update'] = "Обновление ваших файлов";
+$GLOBALS['TL_LANG']['easyupdate3']['updated'] = "Обновлены:";
+$GLOBALS['TL_LANG']['easyupdate3']['skipped'] = "Пропущены:";
+$GLOBALS['TL_LANG']['easyupdate3']['error'] = "Ошибки:";
+$GLOBALS['TL_LANG']['easyupdate3']['exclude'] = "Исключены";
+$GLOBALS['TL_LANG']['easyupdate3']['readme']['headline'] = "Прочтите это полностью перед использованием";
+$GLOBALS['TL_LANG']['easyupdate3']['readme']['text1_title'] = "Обновление состоит из 5 шагов:";
+$GLOBALS['TL_LANG']['easyupdate3']['readme']['text1_text']  = 'Получение списка файлов в архиве. Резервное копирование имеющихся файлов. Обновление файлов. Запуск мастера установки. Вполне возможно, что вам будет необходимо обновить базу данных. Если вы удалите файл конфигурации, пароль мастера установки: "contao"';
+$GLOBALS['TL_LANG']['easyupdate3']['readme']['text2_title'] = "Уведомление безопасности:";
+$GLOBALS['TL_LANG']['easyupdate3']['readme']['text2_text']  = "Ничто не является 100% безопасным. Смотрите, что вы делаете. Если вы не уверены, сделайте резервную копию базы данных, прежде чем использовать этот инструмент в первый раз. Инструмент изменяет тип файловой системы, возможна потеря данных. Вы делаете все на ваш страх и риск.";
+$GLOBALS['TL_LANG']['easyupdate3']['readme']['text3']['working']   = "Проверка и запуск: 3.x.x ==> 3.x.x";
+$GLOBALS['TL_LANG']['easyupdate3']['readme']['text3']['incorrect'] = "Проверка и ошибка: 2.x.x ==> 2.x.x";
+$GLOBALS['TL_LANG']['easyupdate3']['readme']['text4'] = "Работа расширения была проверена на Contao версии 3.4.0. Работа с Contao 2.x невозможна!. Если вы видите версию XXX, вполне возможно, что ZIP-архив поврежден.";
+ 
